26984c6942839f1e8dcd86a3ff10625fa28c7d5e
Ferhat Cengiz <fc@aof.anadolu.edu.tr>
2025-05-08 23:29:50 +0300

    fix: update contract address in create, delete, read, and update scripts

--- delete.js
+++ delete.js
@@ -3,7 +3,7 @@ const Web3 = require("web3").default;
 const { abi } = require("./build/HelloWorldSimple.json");
 
 const RPC_URL          = "http://127.0.0.1:7545";
-const CONTRACT_ADDRESS = "0xAE098B6b069dA8031000CA2deCAcaeaeb8229120";
+const CONTRACT_ADDRESS = "0xe72D74187D4B157E09F2f168D03430D48d91aFE3";
 
 const web3     = new Web3(RPC_URL);
 const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
